62a0eb6d55cfb1cb4f27133bf2615b1673f4404b
ManILoveFishing <105947538+ManILoveFishing@users.noreply.github.com>
2022-07-14 23:24:55 -0700

    Done

---
+++ script.js
@@ -0,0 +1,21 @@
+const panels = document.querySelectorAll('.panel')
+
+panels.forEach((panel) => {
+    panel.addEventListener('click', () => {
+        removeActiveClasses()
+        panel.classList.add('active')
+    })
+})
+
+
+function removeActiveClasses() {
+    panel.forEach(panel => {
+        panel.classList.remove('active')
+    })
+ }
+
+ function removeActiveClasses() {
+    panels.forEach(panel =>{
+
+    })
+ }
